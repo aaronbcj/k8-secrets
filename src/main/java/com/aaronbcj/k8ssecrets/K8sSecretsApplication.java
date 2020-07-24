373ba23122db58bbe579b4581999e422b5be04bb
Aaron Balson <aaronbcj@yahoo.co.in>
2020-07-24 17:14:15 +0530

    Initial

---
+++ src/main/java/com/aaronbcj/k8ssecrets/K8sSecretsApplication.java
@@ -0,0 +1,13 @@
+package com.aaronbcj.k8ssecrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sSecretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sSecretsApplication.class, args);
+	}
+
+}
